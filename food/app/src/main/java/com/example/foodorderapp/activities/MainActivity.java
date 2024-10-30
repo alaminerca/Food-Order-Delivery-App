9c71889a288db54dbf2b8b7b60116e13f647965d
ALAMINE MOUHAMAD <alaminerca@gmail.com>
2024-10-31 00:36:11 +0300

    Project setup with bottom navigation and basic fragments

---
+++ food/app/src/main/java/com/example/foodorderapp/activities/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.foodorderapp.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.foodorderapp.R;
+import com.example.foodorderapp.fragments.MenuFragment;
+import com.example.foodorderapp.fragments.CartFragment;
+import com.example.foodorderapp.fragments.OrdersFragment;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.navigation_menu) {
+                selectedFragment = new MenuFragment();
+            } else if (itemId == R.id.navigation_cart) {
+                selectedFragment = new CartFragment();
+            } else if (itemId == R.id.navigation_orders) {
+                selectedFragment = new OrdersFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+                return true;
+            }
+            return false;
+        });
+
+        // Set default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new MenuFragment())
+                    .commit();
+        }
+    }
+}
---
+++ food/app/src/main/java/com/example/foodorderapp/fragments/OrdersFragment.java
@@ -0,0 +1,17 @@
+package com.example.foodorderapp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import com.example.foodorderapp.R;
+
+public class OrdersFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_orders, container, false);
+    }
+}
